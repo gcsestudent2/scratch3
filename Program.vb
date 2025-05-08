8877944456ab78e17a89c64c2156dbc8d2f21035
gcsestudent2 <gcsestudent2@gmail.com>
2025-05-08 22:03:35 +0000

    steps_from_empty

---
+++ Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Stop
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
--- c#/src2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-for (int i = 0; i<100; i++) {
-    Thread.Sleep(1000);
-    Console.WriteLine("Hello, World!");
-}
