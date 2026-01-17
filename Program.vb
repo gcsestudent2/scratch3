970c6d983e58ea5f867e8780bfb48cc5d64c4a9c
gcsestudent2 <gcsestudent2@gmail.com>
2026-01-17 15:55:11 +0000

    update from net8.0 to net10.0.0

--- Program.vb
+++ Program.vb
@@ -5,19 +5,18 @@ Imports System
 Module Program
     Sub Main(args As String())
         ' ============ Top of editable area (don't change anything above this line)
-       console.writeline ("enter email")
-       dim emailadress as string
-       emailadress =console.readline()
-       console.writeline ("enter the same email again")
-       dim emailadress2 as string 
-       emailadress2=console.readline()
-       if emailadress = emailadress2 then 
-         console.writeline("match")
-       else 
-        console.writeline (" do not match")
-
-       end if     
-
+        'Stop
+        console.writeline ("enter email")
+        dim emailadress as string
+        emailadress = console.readline()
+        console.writeline ("enter the same email again")
+        dim emailadress2 as string 
+        emailadress2= console.readline()
+        if emailadress = emailadress2 then
+            console.writeline("match")
+        else 
+            console.writeline ("do not match")
+        end if     
         ' ============ Bottom of editable area (don't change anything below this line)
     End Sub
 End Module
